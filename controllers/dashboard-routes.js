164ff848a448a08b5decce6f5fb3bac24c8245e6
ana <ana@anniecannons.com>
2023-01-10 21:41:51 -0800

    added the partial handlebars

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -10,17 +10,11 @@ router.get("/", withAuth, async (req, res) => {
       where: {
         user_id: req.session.user_id,
       },
-      attributes: ["id", "post_text", "title",],
+      attributes: ["id", "post_text", "title"],
       include: [
         {
           model: Comment,
-          attributes: [
-            "id",
-            "comment_text",
-            "post_id",
-            "user_id",
-         
-          ],
+          attributes: ["id", "comment_text", "post_id", "user_id"],
           include: {
             model: User,
             attributes: ["name"],
@@ -44,52 +38,58 @@ router.get("/", withAuth, async (req, res) => {
 });
 
 // get one post
-router.get("/edit/:id", withAuth, async (req, res) => {
-  try {
-    const postData = await Post.findByPk(req.params.id, {
-      attributes: ["id", "post_text", "title", "created_at"],
-      include: [
-        {
-          model: Comment,
-          attributes: [
-            "id",
-            "comment_text",
-            "post_id",
-            "user_id",
-           
-          ],
-          include: {
-            model: User,
-            attributes: ["name"],
+router.get('/edit/:id', withAuth, (req, res) => {
+  Post.findOne({
+          where: {
+              id: req.params.id
           },
-        },
-        {
-          model: User,
-          attributes: ["name"],
-        },
-      ],
-    });
-    const post = postData.get({ plain: true });
-    res.render("edit-post", { post, loggedIn: true });
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
+          attributes: [
+              'id',
+              'title',
+              'content',
+              'created_at'
+          ],
+          include: [{
+                  model: Comment,
+                  attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                  include: {
+                      model: User,
+                      attributes: ['username']
+                  }
+              },
+              {
+                  model: User,
+                  attributes: ['username']
+              }
+          ]
+      })
+      .then(dbPostData => {
+          if (!dbPostData) {
+              res.status(404).json({
+                  message: 'No post found with this id'
+              });
+              return;
+          }
 
-//  this is the route to get new post
-router.get("/new", withAuth, async (req, res) => {
-  try {
-    const postData = await Post.findAll({
-      attributes: { exclude: ["password"] },
-      where: {
-        id: req.session.user_id,
-      },
-    });
-    const posts = postData.map((post) => post.get({ plain: true }));
-    res.render("new-post", { posts, loggedIn: true });
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
+          const post = dbPostData.get({
+              plain: true
+          });
+
+          res.render('edit-post', {
+              post,
+              loggedIn: true
+          });
+      })
+      .catch(err => {
+          console.log(err);
+          res.status(500).json(err);
+      });
+})
+
+router.get('/new', (req, res) => {
+  res.render('add-post', {
+      loggedIn: true
+  })
+})
 
 module.exports = router;
--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -80,47 +80,8 @@ router.get("/post/:id", async (req, res) => {
 });
 
 // get all posts for dashboard
-router.get("/dashboard", withAuth, async (req, res) => {
-  try {
-    const postData = await Post.findAll({
-      where: {
-        user_id: req.session.user_id,
-      },
-      attributes: ["id", "post_text", "title"],
-      include: [
-        {
-          model: User,
-          attributes: ["name"],
-        },
-        {
-          model: Comment,
 
-          attributes: [
-            "id",
-            "comment_text",
-            "post_id",
-            "user_id",
-            "created_at",
-          
-          ],
-          include: {
-            model: User,
-            attributes: ["name"],
-          },
-        },
-      ],
-    });
-
-    const posts = postData.map((post) => post.get({ plain: true }));
-
-    res.render("dashboard", {
-      posts,
-      logged_in: req.session.logged_in,
-    });
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
+    
 //  
 router.get("/login", (req, res) => {
   if (req.session.loggedIn) {
