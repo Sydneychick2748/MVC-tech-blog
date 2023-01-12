d596086a89a7da74aec700d3f4e6ee9e86aa5f15
ana <ana@anniecannons.com>
2023-01-12 13:44:13 -0800

    trying to get my log in working

--- models/index.js
+++ models/index.js
@@ -3,31 +3,31 @@ const Post = require('./Post');
 const Comment = require('./Comment');
 
 User.hasMany(Post, {
-    foreignKey: 'user_id', 
+    foreignkey: 'user_id', 
 });
 
 Post.belongsTo(User, {
-    foreignKey: 'user_id',
+    foreignkey: 'user_id',
     onDelete: 'SET NULL',
 });
 
 Comment.belongsTo(User, {
-    foreignKey: 'user_id',
+    foreignkey: 'user_id',
     onDelete: 'SET NULL',
 })
 
 Comment.belongsTo(Post, {
-    foreignKey: 'post_id',
+    foreignkey: 'post_id',
     onDelete: 'SET NULL',
 });
 
 User.hasMany(Comment, {
-    foreignKey: 'user_id',
+    foreignkey: 'user_id',
     onDelete: 'SET NULL',
 });
 
 Post.hasMany(Comment, {
-    foreignKey: 'post_id',
+    foreignkey: 'post_id',
 })
 
 module.exports = { User, Post, Comment };
